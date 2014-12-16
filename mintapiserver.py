09d979cd5702448fa66bdd4d959724175aa1a0d7
Ian Levesque <ian@ianlevesque.org>
2014-12-16 18:32:53 -0500

    Add some caching.

--- mintapiserver.py
+++ mintapiserver.py
@@ -4,15 +4,22 @@ import mintapi
 from helpers import requires_auth
 
 from flask import Flask, jsonify, request, Response
+from flask.ext.cacheify import init_cacheify
 
 app = Flask(__name__)
+cache = init_cacheify(app)
+
+@cache.memoize(1800)
+def get_accounts_from_mint(username, password):
+    return mintapi.get_accounts(username, password)
 
 @app.route('/accounts')
 @requires_auth
 def get_accounts():
     username = request.authorization.username
     password = request.authorization.password
-    accounts = mintapi.get_accounts(username, password)
+    
+    accounts = get_accounts_from_mint(username, password)
     return jsonify(accounts=accounts)
 
 if __name__ == '__main__':
