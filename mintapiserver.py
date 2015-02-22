b254a4031705c2c7986cebd5a346f9c9c84934ee
Ian Levesque <ian@ianlevesque.org>
2015-02-22 03:12:07 -0500

    Can fetch transactions, reports incorrect mint credentials with a 401 instead of 500, preliminary attempt to cache the session token (disabled).

--- mintapiserver.py
+++ mintapiserver.py
@@ -9,9 +9,44 @@ from flask.ext.cacheify import init_cacheify
 app = Flask(__name__)
 cache = init_cacheify(app)
 
+class InvalidCredentials(Exception):
+    def __init__(self, message):
+        self.message = message
+        Exception.__init__(self)
+
+@app.errorhandler(InvalidCredentials)
+def handle_invalid_usage(error):
+    return Response(error.message + '\n', 401, {'WWW-Authenticate': 'Basic realm="Mint credentials (retry)"'})
+
+# def mintapi_token(username, password):
+#     return mintapi.Mint(username, password).token
+
+def mintapi_instance(username, password):
+    try:
+        mint = mintapi.Mint(username, password)
+        # mint.token = mintapi_token(username, password)
+        return mint
+    except Exception, e:
+        if 'login' in str(e):
+            raise InvalidCredentials(str(e))
+        else:
+            raise
+
 @cache.memoize(1800)
-def get_accounts_from_mint(username, password):
-    return mintapi.get_accounts(username, password)
+def fetch_accounts(username, password):
+    return mintapi_instance(username, password).get_accounts()
+
+@cache.memoize(1800)
+def fetch_transactions(username, password):
+    return mintapi_instance(username, password).get_transactions(False)
+
+@app.route('/')
+@requires_auth
+def get_token():
+    username = request.authorization.username
+    password = request.authorization.password
+    
+    return jsonify(token=mintapi_instance(username,password).token)
 
 @app.route('/accounts')
 @requires_auth
@@ -19,8 +54,15 @@ def get_accounts():
     username = request.authorization.username
     password = request.authorization.password
     
-    accounts = get_accounts_from_mint(username, password)
-    return jsonify(accounts=accounts)
+    return jsonify(accounts=fetch_accounts(username,password))
+
+@app.route('/transactions')
+@requires_auth
+def get_transactions():
+    username = request.authorization.username
+    password = request.authorization.password
+    
+    return Response(fetch_transactions(username,password).to_csv(index=False), 200, {'Content-Type': 'text/csv'})
 
 if __name__ == '__main__':
     app.run(debug=True,port=8888)
